6861a9c5764beacc976d392b26eea46c8e07a0b6
rotobogdan <protobogdan@gmail.com>
2020-09-14 09:40:24 +0300

    Create Types (prevent annoying errors)

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -1,4 +1,4 @@
 export const IMPORT_CSV = 'import_csv';
-export const CREATE_CSV = 'import_csv';
-export const UPDATE_CSV = 'import_csv';
-export const DELETE_CSV = 'import_csv';
+export const CREATE_CSV = 'create_csv';
+export const UPDATE_CSV = 'update_csv';
+export const DELETE_CSV = 'delete_csv';
